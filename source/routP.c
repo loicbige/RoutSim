dea63e7ab829e4bc3ae52250502dc1ad9b1055d4
loic <loic.bige@univ-tlse3.fr>
2026-01-13 16:35:25 +0100

    createroutingtable & new config files

--- source/routP.c
+++ source/routP.c
@@ -15,13 +15,36 @@
 #define BUF_SIZE 64        // we should receive less...
 #define IPV4_ADR_STRLEN 16 // == INET_ADDRSTRLEN
 #define LOCALHOST "127.0.0.1"
-#define NO_BASE_PORT 17900
+#define DASHBOARD_PORT 9999
+#define DASHBOARD_IP "127.0.0.1"
 
-
-
-int main(int argc, char const *argv[])
+void createRoutingTable(routingTable *myRoutingTable, const char *adr, const char *id)
 {
 
+  char idInitConfigFile[20]; // Id of the configuration file of the router
+  char myId[32];
+
+  /* Building ID Router from command args */
+  sprintf(myId, "R%s %s", id, adr);
+  printf("[ROUTEUR] : %s\n", myId);
+  // printf("construction id fichier\n");
+  /* Building Config File ID from command args */
+  sprintf(idInitConfigFile, "config/rf%s", id);
+  strcat(idInitConfigFile, ".txt");
+  // printf("\n Nom fichier Configuration : %s",idInitConfigFile);
+  /* Loading My Routing Table from Initial Config file */
+  init_routingTable(myRoutingTable, idInitConfigFile);
+  printf("[ROUTEUR] : %d entrées initialement chargées \n", myRoutingTable->nb_entry);
+  display_routingTable(myRoutingTable);
+}
+sendtoDashboard(char *)
+int main(int argc, char const *argv[])
+{
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s <ip> <id> <port>\n", argv[0]);
+  }
+  routingTable rt;
+  createRoutingTable(&rt, argv[1], argv[2]);
 
   return 1;
 }
